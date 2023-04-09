63be766326f88222e47be1e7f77a6b903d0aa42e
Alexa Hendry <alexahendry88@gmail.com>
2023-04-09 22:46:02 +0000

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import Container from "react-bootstrap/Container";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
         <NavBar />
+        <Container className={styles.Main}>
+          <Switch>
+            <Route exact path="/" render={() => <h1>Homepage</h1>} />
+            <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+            <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+            <Route render={() => <p>Page not found!</p>} />
+          </Switch>
+        </Container>
     </div>
   );
 }
