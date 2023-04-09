992ade6c93640c6eaddb69bb873dca76ba7c8a99
Alexa Hendry <alexahendry88@gmail.com>
2023-04-09 20:25:12 +0000

    Install Bootstrap, font awesome script, create first component NavBar, update favicon and logo

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <i className="fa-solid fa-headset"></i>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i>Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
