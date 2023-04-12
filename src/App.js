93af42eae1a023896bcd02dee4cfeac55727252d
Alexa Hendry <alexahendry88@gmail.com>
2023-04-12 20:18:52 +0000

    Create PostsPage

--- src/App.js
+++ src/App.js
@@ -7,14 +7,39 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Homepage</h1>} />
+        <Route
+            exact
+            path="/"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust the search keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/events"
+            render={() => (
+              <div>
+                <h1>Events Page</h1>
+                <p>Search results message and filter like on homepage</p>
+              </div>
+            )}
+          />
+          <Route exact path="/chat" render={() => <h1>Chat Page</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
