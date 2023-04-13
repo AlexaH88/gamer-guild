3c564bf892599746785dc398576918b99e2173b5
Alexa Hendry <alexahendry88@gmail.com>
2023-04-13 14:29:32 +0000

    Add edit and delete functionality dropdown on posts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import { Route, Switch } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
-import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
@@ -19,7 +19,7 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-        <Route
+          <Route
             exact
             path="/"
             render={() => (
