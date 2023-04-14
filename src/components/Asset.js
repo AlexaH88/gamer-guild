b3f1e52a88939a8af59deca8edf872ce560a9d04
Alexa Hendry <alexahendry88@gmail.com>
2023-04-14 13:46:43 +0000

    Add search bar functionality to posts

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -5,7 +5,7 @@ import styles from "../styles/Asset.module.css";
 const Asset = ({ spinner, src, message }) => {
   return (
     <div className={`${styles.Asset} p-4`}>
-      {spinner && <Spinner animation="border" />}
+      {spinner && <Spinner className={styles.Spinner} animation="border" />}
       {src && <img src={src} alt={message} />}
       {message && <p className="mt-4">{message}</p>}
     </div>
