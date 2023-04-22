7cd12a33bea7220fdee78ed43ec4247af6c8be37
Alexa Hendry <alexahendry88@gmail.com>
2023-04-22 16:46:05 +0000

    Ensure full width on AddContentButton by addind Container css

--- src/components/AddContentButton.js
+++ src/components/AddContentButton.js
@@ -6,7 +6,7 @@ import btnStyles from "../styles/Button.module.css";
 // code taken from whereisthemouse.com and adapted --> More info in README
 const AddContentButton = ({ url, text, mobile }) => {
   return (
-    <Container className={`${mobile && "d-lg-none text-center mb-3 m-0 p-0"}`}>
+    <Container className={`${btnStyles.Container} ${mobile && "d-lg-none text-center mb-3 m-0 p-0"}`}>
       {mobile ? (
         <Button
           className={`d-flex justify-content-around ${btnStyles.AddContentButton} ${btnStyles.Button} ${btnStyles.Blue} ${btnStyles.Wide}`}
